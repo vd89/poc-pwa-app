734cbcfc59ac4ecdc622e9f0e388aa9ad206397f
dixitVora <dixitvora@gmail.com>
2023-12-31 16:20:47 +0530

    main: added the initail setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
     </div>
   );
 }
